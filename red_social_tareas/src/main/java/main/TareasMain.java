1001c83019c43a38e8525bb755f0d448373d5027
ikasle <david>
2026-01-12 13:00:52 +0100

    Creacion de proyecto

---
+++ red_social_tareas/src/main/java/main/TareasMain.java
@@ -0,0 +1,21 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TareasMain extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/escenas/vista.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
