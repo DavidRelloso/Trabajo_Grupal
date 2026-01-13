1e3153d7ecfb846054569845989cd1575e6d269a
Ikasle Arangoya <ikasle@arangoya.local>
2026-01-13 14:21:20 +0100

    Creacion interfaz principal - Garazi

---
+++ red_social_tareas/src/main/java/main/ControladorTareasMain.java
@@ -0,0 +1,18 @@
+package main;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+
+public class ControladorTareasMain {
+
+	Button botonCerrarSesion;
+	MenuButton diarioAbrir, amigosAbrir, ajustesAbrir;
+	
+	public void initialize() {
+		System.out.println("Entra");
+	}
+	
+	public void abrirDiario() {
+		
+	}
+}
