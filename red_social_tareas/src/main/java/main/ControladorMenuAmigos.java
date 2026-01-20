4bd5b9540c8d87ce409959b5d81489b1163edd77
Ikasle Arangoya <ikasle@arangoya.local>
2026-01-20 13:36:45 +0100

    Update menuAmigos -Garazi

--- red_social_tareas/src/main/java/main/ControladorMenuAmigos.java
+++ red_social_tareas/src/main/java/main/ControladorMenuAmigos.java
@@ -1,16 +1,24 @@
 package main;
 import java.io.IOException;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class ControladorMenuAmigos {
 
 	public class MenuAmigos {
 
 		Button agregarAmigoBtn, peticonesAmistadBtn, misAmigosBtn;
+		Stage stage;
 		
-		public void abrirMenuAmigos() throws IOException {
-		    
+		public void abrirMenuAgregarAmigos() throws IOException {
+		        FXMLLoader loader = new FXMLLoader(getClass().getResource("/escenas/menuAmigos.fxml"));
+		        Scene scene = new Scene(loader.load());
+		        stage.setScene(scene);
+		        stage.show();
+		    }
 		}
 	}
-}
+
